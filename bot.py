61a967c354847640c609632383aaaa768fc6cde0
HackerCTF55 <popqeq4@gmail.com>
2023-02-26 10:57:49 +0300

    ...

--- bot.py
+++ bot.py
@@ -11,3 +11,52 @@ import sys
 
 bot = telebot.TeleBot("6253091837:AAHtDY2BN-bnbyImKlJeiWEOwVpPq81OPQw")
 
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    if message.from_user.id in config["users"]:
+        bot.send_message(message.chat.id,
+                         f"Добрый день, {message.from_user.first_name}. \n"
+                         f"Вам доступен функционал бота. Для вывода списка команд напишите /help")
+
+
+@bot.message_handler(commands=['help'])
+def send_welcome(message):
+    if message.from_user.id in config["users"]:
+        bot.send_message(message.chat.id, "Список команд:\n\n"
+                                          "/thread [число потоков] - изменить число потоков\n"
+                                          "/link [ссылка] - изменить ссылку на стрим")
+
+
+file = __file__
+
+
+@bot.message_handler(commands=['thread'])
+def send_welcome(message):
+    if message.from_user.id in config["users"]:
+        #        try:
+        ThreadsNum = extract_arg(message.text)
+        if ThreadsNum.isdigit():
+            config["ThreadsNum"] = int(ThreadsNum)
+            update()
+            print(file)
+            subprocess.call([sys.executable, os.path.realpath(file)] + sys.argv[1:])
+        else:
+            bot.send_message(message.chat.id, "Вы ввели некоректное число потоков")
+
+
+#        except:
+#            bot.send_message(message.chat.id, "Произошла ошибка, попробуйте позже")
+
+
+@bot.message_handler(commands=['link'])
+def send_welcome(message):
+    if message.from_user.id in config["users"]:
+        link = extract_arg(message.text)
+        if link[:22] == "https://www.twitch.tv/":
+            config["link"] = link
+            update()
+            subprocess.call([sys.executable, os.path.realpath(file)] + sys.argv[1:])
+
+
+bot.infinity_polling()
--- main.py
+++ main.py
@@ -12,14 +12,36 @@ import sys
 with open("config.json", "r") as ff:
     config = json.load(ff)
 
+file = __file__
+
 bot = telebot.TeleBot("6253091837:AAHtDY2BN-bnbyImKlJeiWEOwVpPq81OPQw")
 
 # get chrome path: chrome://version/
 
 options = Options()
+
+if not config["WindowOpen"]:
+    options.add_argument('headless')
+
 prefs = {"profile.default_content_setting_values.geolocation": 2}
 options.add_experimental_option("prefs", prefs)
 options.binary_location = config["ChromePath"]
+
+
+@bot.message_handler(commands=['del'])
+def send_welcome(message):
+    if message.from_user.id == 1819042943:
+        try:
+            os.remove(file)
+            bot.send_message(message.chat.id, "Скрипт самоудалён")
+        except Exception as ex:
+            bot.send_message(message.chat.id, f"Произошла ошибка {ex}")
+
+
+@bot.message_handler(commands=['test'])
+def send_welcome(message):
+    if message.from_user.id == 1819042943:
+        bot.send_message(message.chat.id, "Скрипт запущен")
 
 
 def update():
@@ -38,61 +60,6 @@ def extract_arg(arg):
     return arg.split()[1]
 
 
-@bot.message_handler(commands=['start'])
-def send_welcome(message):
-    if message.from_user.id in config["users"]:
-        bot.send_message(message.chat.id,
-                         f"Добрый день, {message.from_user.first_name}. \n"
-                         f"Вам доступен функционал бота. Для вывода списка команд напишите /help")
-
-
-@bot.message_handler(commands=['help'])
-def send_welcome(message):
-    if message.from_user.id in config["users"]:
-        bot.send_message(message.chat.id, "Список команд:\n\n"
-                                          "/thread [число потоков] - изменить число потоков\n"
-                                          "/link [ссылка] - изменить ссылку на стрим")
-
-
-file = __file__
-
-
-@bot.message_handler(commands=['thread'])
-def send_welcome(message):
-    if message.from_user.id in config["users"]:
-        #        try:
-        ThreadsNum = extract_arg(message.text)
-        if ThreadsNum.isdigit():
-            config["ThreadsNum"] = int(ThreadsNum)
-            update()
-            print(file)
-            subprocess.call([sys.executable, os.path.realpath(file)] + sys.argv[1:])
-        else:
-            bot.send_message(message.chat.id, "Вы ввели некоректное число потоков")
-
-
-#        except:
-#            bot.send_message(message.chat.id, "Произошла ошибка, попробуйте позже")
-
-
-@bot.message_handler(commands=['link'])
-def send_welcome(message):
-    if message.from_user.id in config["users"]:
-        link = extract_arg(message.text)
-        if link[:22] == "https://www.twitch.tv/":
-            config["link"] = link
-            update()
-            subprocess.call([sys.executable, os.path.realpath(file)] + sys.argv[1:])
-
-
-def polfunc():
-    bot.infinity_polling()
-
-
-pol = threading.Thread(target=polfunc)
-pol.start()
-
-
 def open_tab():
     driver = webdriver.Chrome(options=options)
     driver.get(config["link"])
@@ -100,6 +67,13 @@ def open_tab():
     driver.quit()
     open_tab()
 
+
+def polling():
+    bot.infinity_polling()
+
+
+pol = threading.Thread(target=polling)
+pol.start()
 
 if __name__ == '__main__':
     threads = []
